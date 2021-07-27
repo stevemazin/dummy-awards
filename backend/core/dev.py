3c50103aeb5a59da9c01df9e3e24b6527b56f018
steve mazin <munenekinyua.mk@gmail.com>
2021-07-27 16:30:35 +0300

    Settings fix

---
+++ backend/core/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+DEBUG = True
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+ALLOWED_HOSTS = ['stevemazin.pythonanywhere.com', 'localhost', '127.0. 0.1']
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    'https://stevemazin.pythonanywhere.com'
+]
+
+try:
+    from .credentials import *
+except ImportError:
+    pass
+
