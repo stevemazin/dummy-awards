4bf094f7fd0ec240b58e1bd4c2d793f6d69d10a3
steve mazin <munenekinyua.mk@gmail.com>
2021-07-27 16:37:41 +0300

    Switched back to dev

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.dev')
 
 application = get_wsgi_application()
